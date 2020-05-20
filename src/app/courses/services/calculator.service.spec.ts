c56a92bca64aa52f0ea71b94b0b6eeee371939e6
Igor <slabiydoter@gmail.com>
2020-05-20 19:32:17 +0300

    added services tests

---
+++ src/app/courses/services/calculator.service.spec.ts
@@ -0,0 +1,38 @@
+import { CalculatorService } from "./calculator.service";
+import { TestBed } from "@angular/core/testing";
+import { LoggerService } from "./logger.service";
+
+describe('Calculator service', () => {
+
+    let calculator: CalculatorService,
+        loggerSpy: any;
+
+    beforeEach(() => {
+        loggerSpy = jasmine.createSpyObj('LoggerService', ["log"]);
+
+        TestBed.configureTestingModule({
+            providers: [
+                CalculatorService,
+                {provide: LoggerService, useValue: loggerSpy}
+            ]
+        });
+
+        calculator = TestBed.get(CalculatorService);
+    });
+
+    it('should add two numbers', () => {
+        const result = calculator.add(2, 2);
+
+        expect(result).toBe(4);
+        expect(loggerSpy.log).toHaveBeenCalledTimes(1);
+
+    });
+
+    it('should subtract two numbers', () => {
+        const result = calculator.subtract(2, 2);
+
+        expect(result).toBe(0, "unispected substraction result.");
+        expect(loggerSpy.log).toHaveBeenCalledTimes(1);
+    });
+
+});
